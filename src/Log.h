323ddfce7c3bfdc0eea0019d05bf01c5e8b8821b
daihaonan <1240184197@qq.com>
2019-08-02 20:18:37 +0800

    cause:log add tid solution:done

--- src/Log.h
+++ src/Log.h
@@ -2,13 +2,14 @@
 #define _MELON_LOG_H_
 
 #include <memory>
-#include <vector>
 #include <string>
 #include <sstream>
 #include <sys/types.h>
-#include <time.h>
-
 #include "Singleton.h"
+#include <time.h>
+#include "Thread.h"
+#include <vector>
+
 
 namespace melon {
 
@@ -96,20 +97,20 @@ template class Singleton<Logger>;
 }
 
 #define LOG_DEBUG if (melon::Logger::getLogLevel() <= melon::LogLevel::DEBUG) \
-													  melon::LogWrapper(melon::LogEvent::ptr(new melon::LogEvent(time(nullptr), 1, melon::LogLevel::DEBUG, \
+													  melon::LogWrapper(melon::LogEvent::ptr(new melon::LogEvent(time(nullptr), Thread::CurrentThreadTid(), melon::LogLevel::DEBUG, \
 									__FILE__, __LINE__))).getStream()
 
 #define LOG_INFO if (melon::Logger::getLogLevel() <= melon::LogLevel::INFO) \
-													  melon::LogWrapper(melon::LogEvent::ptr(new melon::LogEvent(time(nullptr), 1, melon::LogLevel::INFO, \
+													  melon::LogWrapper(melon::LogEvent::ptr(new melon::LogEvent(time(nullptr), Thread::CurrentThreadTid(), melon::LogLevel::INFO, \
 									__FILE__, __LINE__))).getStream()
 
-#define LOG_WARN melon::LogWrapper(melon::LogEvent::ptr(new melon::LogEvent(time(nullptr), 1, melon::LogLevel::WARN, \
+#define LOG_WARN melon::LogWrapper(melon::LogEvent::ptr(new melon::LogEvent(time(nullptr), Thread::CurrentThreadTid(), melon::LogLevel::WARN, \
 									__FILE__, __LINE__))).getStream()
 
-#define LOG_ERROR melon::LogWrapper(melon::LogEvent::ptr(new melon::LogEvent(time(nullptr), 1, melon::LogLevel::ERROR, \
+#define LOG_ERROR melon::LogWrapper(melon::LogEvent::ptr(new melon::LogEvent(time(nullptr), Thread::CurrentThreadTid(), melon::LogLevel::ERROR, \
 									__FILE__, __LINE__))).getStream()
 
-#define LOG_FATAL melon::LogWrapper(melon::LogEvent::ptr(new melon::LogEvent(time(nullptr), 1, melon::LogLevel::FATAL, \
+#define LOG_FATAL melon::LogWrapper(melon::LogEvent::ptr(new melon::LogEvent(time(nullptr), Thread::CurrentThreadTid(), melon::LogLevel::FATAL, \
 									__FILE__, __LINE__))).getStream()
 
 #endif
