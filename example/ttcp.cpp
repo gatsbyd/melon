91ae09c40d564b21f77a6f8f4ce45d0d1601e01b
root <1240184197@qq.com>
2019-09-27 21:53:56 +0800

    cause:example parse ttcp option

--- example/ttcp.cpp
+++ example/ttcp.cpp
@@ -4,6 +4,7 @@
 
 #include <stdio.h>
 #include <stdint.h>
+#include <string.h>
 
 using namespace melon;
 
@@ -11,29 +12,64 @@ struct Cmd {
 	bool server;
 	bool client;
 	uint16_t port;
-	char* ip;
+	std::string ip;
 	int length;
 	int number;
 
 	Cmd()
-		: server(false), client(false), port(0), ip(nullptr),
-			length(0), number(0) {
+		:server(false), client(false), port(5000), 
+			length(65536), number(1024) {
 	}
 
 };
 
 void printUsage(char* app) {
-	printf("server side:%s s port length number\n \
-			client size:%s c ip port length number", app, app);
+	printf("server side:%s -s port length number\n \
+			client size:%s -c ip port length number", app, app);
 }
 
 bool parseCmd(int argc, char* argv[], Cmd& cmd) {
-
+	if (argc < 2) {
+		printUsage(argv[0]);
+		return false;
+	}
+	if (strcmp("-s", argv[1]) == 0) {
+		cmd.server = true;
+		if (argc > 2) {
+			cmd.port = atoi(argv[2]);
+		}
+		if (argc > 3) {
+			cmd.length = atoi(argv[3]);
+		}
+		if (argc > 4) {
+			cmd.number = atoi(argv[4]);
+		}
+	} else if (strcmp("-c", argv[1]) == 0) {
+		cmd.client = true;
+		if (argc > 2) {
+			cmd.ip = argv[2];
+		} else {
+			printUsage(argv[0]);
+			return 0;
+		}
+		if (argc > 3) {
+			cmd.port = atoi(argv[2]);
+		}
+		if (argc > 4) {
+			cmd.length = atoi(argv[3]);
+		}
+		if (argc > 5) {
+			cmd.number = atoi(argv[4]);
+		}
+	} else {
+		printUsage(argv[0]);
+		return false;
+	}
 	return true;
 }
 
 void transmit(TcpConnection::Ptr conn) {
-
+	
 };
 
 void receive(TcpConnection::Ptr conn) {
