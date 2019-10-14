8b166084a2ac73354be2d41233d2404a91f4b8e2
root <1240184197@qq.com>
2019-10-14 13:15:48 +0800

    cause:trivial commit

--- test/HttpServer_test.cpp
+++ test/HttpServer_test.cpp
@@ -1,13 +1,29 @@
-#include "http/HttpServer.h"
+#include "http/HttpConnection.h"
+#include "TcpServer.h"
 #include "Scheduler.h"
 #include "Log.h"
 
-int main() {
-	melon::Singleton<melon::Logger>::getInstance()->addAppender("console", melon::LogAppender::ptr(new melon::ConsoleAppender()));
+using namespace melon;
+using namespace melon::http;
 
-	melon::IpAddress addr(80);
-	melon::http::HttpServer server(addr);
+int main() {
+	Singleton<Logger>::getInstance()->addAppender("console", LogAppender::ptr(new ConsoleAppender()));
+
+	Scheduler scheduler;
+	IpAddress addr(80);
+	TcpServer server(addr, &scheduler);
+	server.setConnectionHandler([](TcpConnection::Ptr conn) {
+					HttpConnection::Ptr http_conn = std::make_shared<HttpConnection>(conn);
+					HttpRequest::Ptr request = http_conn->recvRequest();
+
+					HttpResponse::Ptr rsp = std::make_shared<HttpResponse>();
+					rsp->setHttpStatus(HttpStatus::OK);
+					rsp->setHeader("Content-Length", "5");
+					rsp->setContent("hello");
+					http_conn->sendResponse(rsp);
+				});
 
 	server.start();
+	scheduler.start();
 	return 0;
 }
